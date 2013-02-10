39da2572be093cdb9ae8dfea35d76ca899c1912e
Denis Usanov <usanovdd@gmail.com>
2013-02-10 17:49:25 +0400

    Add skeleton

---
+++ avp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding= utf-8 -*-
+
+
+actions = {}
+
+actions.convert      = True
+actions.cut          = True
+actions.delogo       = True
+actions.watermarking = True
+actions.thumbnails   = True
+
+
+# video options
+width   = 600
+height  = 450
+bitrate = 1800
+
+
+# cutting options
+startoffset = 10 #in seconds
+
+
+# watermarking options
+logotext = "http://asd.com"
+
