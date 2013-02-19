00c6c959e7bf346d8ab1e74b09ce5744296ac558
Andrew Lapin <andrew.d.lapin@gmail.com>
2013-02-20 01:09:32 +0400

    Implemented function to get set of frames from the video based on opencv

--- avp.py
+++ avp.py
@@ -5,25 +5,55 @@ import config as c
 
 import opencv.cv as cv
 import opencv.highgui as highgui
-import sys
 
-filename = "./tests/test1.mp4"
-#filename = "./tests/test.avi"
+import numpy as np
+
+#filename = "./tests/test1.mp4"
+filename = "./tests/test.avi"
+
+def get_frame_set(file_name, dt, max_frame_count = 10):
+	source = highgui.cvCreateFileCapture(file_name)
+	frame = highgui.cvQueryFrame(source)
+	
+	i = 0
+	
+	frames = []
+
+	while(True):
+		if len(frames) >= max_frame_count:
+			break
+
+		frame = highgui.cvQueryFrame(source)
+		
+		if (frame == None):
+			break
+
+		if i % dt == 0:
+			frames.append(cv.cvCloneImage(frame))
+			
+		i+=1
+
+	return frames
+
+def get_picture_std(img):
+	return np.array(img).std(axis=1).std()
 
 if __name__ == '__main__':
-	highgui.cvNamedWindow("Example2", highgui.CV_WINDOW_AUTOSIZE)
-	capture = highgui.cvCreateFileCapture(filename)
-	frame = highgui.cvQueryFrame(capture)
+	highgui.cvNamedWindow("frame", highgui.CV_WINDOW_AUTOSIZE)
 
 	loop = True
+
+	frames = get_frame_set(filename, 5, 10)
+	for i in range(len(frames)):
+		print "frame:", i, "std:", get_picture_std(frames[i])
+
 	while(loop):
-		frame = highgui.cvQueryFrame(capture)
-		if (frame == None):
-			break;
-		highgui.cvShowImage("Example2", frame)
-		char = highgui.cvWaitKey(33)
-		if (char != -1):
-			if (ord(char) == 27):
-				loop = False
-	 
-	highgui.cvDestroyWindow("Example2")
+		for frame in frames:
+			
+			highgui.cvShowImage("frame", frame)
+			char = highgui.cvWaitKey(33)
+			if (char != -1):
+				if (ord(char) == 27):
+					loop = False
+		
+	highgui.cvDestroyAllWindows()
